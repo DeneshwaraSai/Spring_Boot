e9232ab7c107916d473505d89fbb75bfa0b56a51
DeneshwaraSai <iladenesh99@gmail.com>
2024-05-21 00:16:41 -0500

    Pushing new Spring basic Maven project.

---
+++ SpringBootBasics/src/main/java/com/code/learning/SpringBootBasics/SpringBootBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.code.learning.SpringBootBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicsApplication.class, args);
+	}
+
+}
